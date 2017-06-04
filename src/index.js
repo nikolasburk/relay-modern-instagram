36f702b068254585271d91327affaed5df0efd5f
Nikolas Burk <nikolas.burk@gmail.com>
2017-06-04 12:51:33 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import CreatePage from './CreatePage'
+import registerServiceWorker from './registerServiceWorker'
+import { Router, Route, browserHistory } from 'react-router'
+
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route path='/' component={App} />
+    <Route path='/create' component={CreatePage} />
+  </Router>
+  , document.getElementById('root')
+)
+
+registerServiceWorker()
